02145fa94e7c0d2abbed7ad6f4817e8a62209165
AKotov-dev <alex_q_2000@mail.ru>
2021-11-06 22:00:56 +0300

    v0.3 - code optimization

--- luntikwg/luntikwg.pas
+++ luntikwg/luntikwg.pas
@@ -14,7 +14,7 @@ uses
 
 begin
   RequireDerivedFormResource:=True;
-  Application.Title:='LuntikWG v0.2';
+  Application.Title:='LuntikWG v0.3';
   Application.Scaled:=True;
   Application.Initialize;
   Application.CreateForm(TMainForm, MainForm);
