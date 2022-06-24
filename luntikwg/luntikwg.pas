e1936e65db44cedecd320f6f2f123eeb0aa07e51
AKotov-dev <alex_q_2000@mail.ru>
2022-06-24 13:20:11 +0300

    v0.5 + Improved DNS Leak exclusion mechanism

--- luntikwg/luntikwg.pas
+++ luntikwg/luntikwg.pas
@@ -14,7 +14,7 @@ uses
 
 begin
   RequireDerivedFormResource:=True;
-  Application.Title:='LuntikWG v0.4';
+  Application.Title:='LuntikWG v0.5';
   Application.Scaled:=True;
   Application.Initialize;
   Application.CreateForm(TMainForm, MainForm);
